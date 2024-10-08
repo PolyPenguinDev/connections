f54ed640c63b5c96e5474c51c8bb7cc5a933bea4
PolyPenguin <56366826+PolyPenguinDev@users.noreply.github.com>
2024-10-07 21:44:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -45,8 +45,6 @@ function shuffle(array) {
       // Pick a remaining element...
       let randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
-      // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
